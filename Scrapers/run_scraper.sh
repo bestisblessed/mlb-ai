e589a3d672a6c95efdcb4ca142950433a3efbda5
bestisblessed <tyler.durette@gmail.com>
2025-04-18 06:57:19 -0400

    working scraper

--- Scrapers/run_scraper.sh
+++ Scrapers/run_scraper.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
-# To run the script at 6:30 AM every day, add this line to your crontab:
-# 30 6 * * * /home/trinity/mlb-ai/Scrapers/run_scraper.sh
+# To run the script at 12:01 AM every day, add this line to your crontab:
+# 1 0 * * * /home/trinity/mlb-ai/Scrapers/run_scraper.sh
 
 cd /home/trinity/mlb-ai/Scrapers
 echo "$(date): Starting script execution" >> /home/trinity/mlb-ai/Scrapers/scraper.log 2>&1
 # source .env
 xvfb-run /home/trinity/.pyenv/shims/python ballparkpal_headless.py >> scraper.log 2>&1
 echo "$(date): Script execution completed" >> /home/trinity/mlb-ai/Scrapers/scraper.log 2>&1
+echo "$(date): Removing data/raw directory" >> /home/trinity/mlb-ai/Scrapers/scraper.log 2>&1
+rm -rf data/raw
+git add data/
+git commit -m "Data update $(date +%Y-%m-%d)" >> scraper.log 2>&1
+git push >> scraper.log 2>&1
+echo "$(date): Git push completed" >> scraper.log 2>&1
 
 
 # source /home/trinity/.bashrc  # or .bashrc depending on your setup
